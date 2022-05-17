da802101dc5a046c7a92485753bb7f8da8bb4f19
tinh pham <tinhphamqtkd@gmail.com>
2022-05-17 17:43:05 +0700

    done phan noi dung dang lam pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,79 @@
-
+import {htmlToElement, getDateFromMillisecond as convertMillisecond} from './command.js'
 function loadOrderChildS1(){
 
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            let content = document.getElementsByClassName("content__table__body");
-            const items = JSON.parse(this.responseText);
-            for(let item in items){
-                var value = `
-                    
+            let content = document.getElementsByClassName("content__table");
+            const response = JSON.parse(this.responseText);
+            let lengthResponse = response.data.order.length;
+            for(let i = 0; i < lengthResponse; i++){
+                let item = response.data.order[i];
+                let value = `
+                        <td>
+                            <div class="content__table__header__button-all">
+                                <label class="checkbox-button">
+                                    <input type="checkbox" class="checkbox-button__input">
+                                    <span class="checkbox-button__control"></span>
+                                </label>
+                            </div>
+                        </td>
+                        <td>
+                            <span>${i+1}</span>
+                        </td>
+                        <td>
+                            <span>${convertMillisecond(item.create_date)}</span>
+                        </td>
+                        <td>
+                            <span>${item.user_agency_name}</span>
+                        </td>
+                        <td>
+                            <span>${item.user_agency_code}</span>
+                        </td>
+                        <td>
+                            <span>${item.membership_name}</span>
+                        </td>
+                        <td>
+                            <span>${item.order_code}</span>
+                        </td>
+                        <td>
+                            <span>${item.total_money}</span>
+                        </td>
+                        <td>
+                            <span>${item.status_name}</span>
+                        </td>
+                        <td>
+                            <span>${convertMillisecond(item.receive_order_time)}</span>
+                        </td>
+                        <td>
+                            <span>${item.status_priority}</span>
+                        </td>
+                        <td>
+                            <span>${item.order_creator}</span>
+                        </td>
+                        <td>
+                            <span>${item.agency_name}</span>
+                        </td>
                 `
-                content.appendChild(
-                    `
-                        <h1>helllo okkk</h1>
-                    `
-                )
+
+                let entry = htmlToElement(value, "tr", "content__table__body");
+                content[0].appendChild(entry)
             }
-            console.log(items)
+            setWidthForColumnTable();
         }
     };
     xhttp.open("GET", "http://cmcapp.ipicorp.co:10003/adminservice/order/list_order_agency?startValue=1649955600000&endValue=1652633999999&page=1&status=0&search=&user_agency_id=0&membership_id=0&from_date_temp=1649955600000&to_date_temp=1652633999999");
-    xhttp.setRequestHeader("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbjAzQGdtYWlsLmNvbSIsImlkIjozLCJlbWFpbCI6ImFkbWluMDNAZ21haWwuY29tIiwicm9sZSI6IkFETUlOIiwibmFtZSI6ImFkbWluMDMiLCJ1c2VybmFtZSI6ImFkbWluMDMiLCJleHAiOjE2NTI3MTk0NTR9.D4wuKelPj2c1GIU9nWoi9RuJEQOvVR07-gukDwSh_4o-/order/list_order_agency");
+    xhttp.setRequestHeader("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpdEBhbmh0aW4udm4iLCJpZCI6NTYsImVtYWlsIjoiaXRAYW5odGluLnZuIiwicm9sZSI6IkFETUlOIiwibmFtZSI6IkzDqiDEkHV5biIsInVzZXJuYW1lIjoiaXQiLCJleHAiOjE2NTI4NDE1OTV9.mJJtKViXLrGiqRuJ9xrD3Bl9SNSuZQ02lUdhkMo8Wxs-/order/list_order_agency");
     xhttp.send();
 }
 
+function setWidthForColumnTable(){
+    let elementChildren = document.getElementsByClassName("content__table__header")[0].children;
+    const lengthElement = elementChildren.length;
+    const withElement = 100 / lengthElement + 2 + '%';
+    for(let i = 2; i < lengthElement; i++){
+        elementChildren[i].style.width = withElement;
+    }
+}
+
 loadOrderChildS1();
-
-
